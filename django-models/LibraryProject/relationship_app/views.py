afae17cd93f7f5724752856545532ffe71ed15b7
Yonas Mamo <63198859+yonasi@users.noreply.github.com>
2025-02-23 21:24:45 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 def book_list(request):
     books = Book.objects.all()
