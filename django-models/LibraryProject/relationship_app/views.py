362c162ca2e71470e531e7eecdbd39dc8f1457db
Yonas Mamo <Yonasma416@gmail.com>
2025-02-25 23:09:18 +0300

    part 3 redone

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -52,15 +52,15 @@ def is_member(user):
 
 
 @user_passes_test(is_admin)
-def admin_dashboard(request):
+def Admin(request):
     return render(request, 'admin_view.html')
 
 @user_passes_test(is_librarian)
-def librarian_dashboard(request):
+def Librarian(request):
     return render(request, 'librarian_view.html')
 
 @user_passes_test(is_member)
-def member_dashboard(request):
+def Memberd(request):
     return render(request, 'member_view.html')
 
 
