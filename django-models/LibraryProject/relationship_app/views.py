6e527ea67e9723934767e83554e077efa20b56ef
Yonas Mamo <63198859+yonasi@users.noreply.github.com>
2025-03-02 23:45:17 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -60,7 +60,7 @@ def is_member(user):
         return False
 
 #Admin view
-Admin@user_passes_test(is_admin)
+@user_passes_test(is_admin)
 def Admin(request):
     return render(request, 'admin_view.html', {'message': 'Welcome, Admin!'})
 
