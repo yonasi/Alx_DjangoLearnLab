9937fec6a806c2919f9d635e1b675c8dc77f0132
Yonas Mamo <Yonasma416@gmail.com>
2025-03-08 01:40:43 +0300

    task 2 done

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .import views
+from  api.views import BookViewSet
+
+#Task 2
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    path('books/', views.BookList.as_view()),
+    path('books/', views.BookList.as_view(), name='book-list'),
+    # Task 2
+    path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -8,3 +8,52 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+# Tsdlk 2 ViewSet
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+class BookViewSet(viewsets.ModelViewSet):
+    
+    def list(self, request):
+        queryset = Book.objects.all()
+        serializer_class = BookSerializer(queryset, many=True)
+        return Response(serializer_class.data)
+    
+    def create(self, request):
+        serializer_class = BookSerializer(data=request.data)
+        if serializer_class.is_valid():
+            serializer_class.save()
+            return Response(serializer_class.data, status=status.HTTP_201_CREATED)
+        return Response(serializer_class.errors, status=status.HTTP_404_BAD_REQUEST)
+    
+    def retrieve(self, request, pk=None):
+        try:
+            book = Book.objects.get(pk=pk)
+            serializer_class = BookSerializer(book)
+            return Response(serializer_class.data)
+        
+        except Book.DoesNotExist:
+            return Response({"error": "Book not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    def update(self, request, pk=None):
+        try:
+             book = Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            return Response({'error': 'Book not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer_class = BookSerializer(book, data=request.data)
+        if serializer_class.is_valid():
+            serializer_class.save()
+            return Response(serializer_class.data)
+        return Response(serializer_class.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk=None):
+        try:
+            book = Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            return Response({'error': 'Book not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
