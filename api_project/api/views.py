0d4ebfda6f4d462d5665e23b890105934698c1f7
Yonas Mamo <Yonasma416@gmail.com>
2025-03-07 22:06:23 +0300

    task 1 done

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+# Task 0
+
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
