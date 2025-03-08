0367fc10ce348d448578572e35985b85df1906e3
Yonas Mamo <Yonasma416@gmail.com>
2025-03-08 17:32:07 +0300

    anothor correction on task 3

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .import views
-from  api.views import BookViewSet
+from .views import BookViewSet
+from .views import BookList
 
 #Task 2
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    path('books/', views.BookList.as_view(), name='book-list'),
+    path('books/', BookList.as_view(), name='book-list'),
     # Task 2
     path('', include(router.urls)),
 ]
